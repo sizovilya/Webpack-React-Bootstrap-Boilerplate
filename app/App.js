cdc1445b51b1088b18aeeab4efeab5a73165361e
Ilya Sizov <sizov.ilya@gmail.com>
2017-01-13 16:30:36 +0300

    rm
    removed unused Proptypes

--- app/App.js
+++ app/App.js
@@ -1,6 +1,6 @@
 
 import React from 'react'
-import { Component, PropTypes } from 'react'
+import { Component } from 'react'
 import Button from './Components/Button'
 
 export default class App extends Component {
--- app/Components/Button.js
+++ app/Components/Button.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Component, PropTypes } from 'react'
+import { Component } from 'react'
 
 export default class Button extends Component{
 
