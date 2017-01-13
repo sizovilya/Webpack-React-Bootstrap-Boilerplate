8da84ff92032f2847c6edf4f2631832fd20cb4d7
Ilya Sizov <sizov.ilya@gmail.com>
2017-01-13 16:11:21 +0300

    fixed: Button component

--- app/Components/Button.js
+++ app/Components/Button.js
@@ -5,14 +5,14 @@ export default class Button extends Component{
 
   clickBtn(e){
     e.preventDefault();
-    alert("clicked");
+    alert("Have a good time");
   }
 
   render()
   {
     return (
       <div>
-        <a className="btn btn-success" onClick={this.clickBtn} href="#"> Нажми </a>
+        <a className="btn btn-success" onClick={this.clickBtn} href="#"> Press me <span className="glyphicon glyphicon-music" aria-hidden="true"></span> </a>
       </div>
     )
   }
